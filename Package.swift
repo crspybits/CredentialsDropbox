af7cdae23e75ae17823b6f498308af9e2c2e5556
Christopher Prince <chris@SpasticMuffin.biz>
2017-12-10 18:41:52 -0700

    Going back to 4.0 tools-- resolved linker issue.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,26 @@
+// swift-tools-version:4.0
 // Adapted from https://github.com/IBM-Swift/Kitura-CredentialsGoogle
 
 import PackageDescription
 
 let package = Package(
     name: "CredentialsDropbox",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "CredentialsDropbox",
+            targets: ["CredentialsDropbox"]
+        )
+    ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", majorVersion: 1, minor: 7),
-        ]
+        .package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", .upToNextMinor(from: "1.7.0")),
+        ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "CredentialsDropbox",
+            dependencies: ["Credentials"]
+        )
+    ]
 )
