3be73caa8322e3f7f006661981d43814e1336ca5
Christopher Prince <chris@SpasticMuffin.biz>
2019-06-09 21:08:46 -0600

    Update to Swift 5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // Adapted from https://github.com/IBM-Swift/Kitura-CredentialsGoogle
 
 import PackageDescription
@@ -13,7 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", .upToNextMajor(from: "2.4.0"))
+        .package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", .upToNextMajor(from: "2.4.1"))
         ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
